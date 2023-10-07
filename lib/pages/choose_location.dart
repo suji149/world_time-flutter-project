786d0337d2aa4e7b9f685cff27553ca4a55f457f
suji149 <147227030+suji149@users.noreply.github.com>
2023-10-07 19:34:23 +0530

    changes

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,11 +9,6 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
 
 
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     print('build function ran');
